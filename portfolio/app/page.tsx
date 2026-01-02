4e6e6773447979c6b2067c3d7303429a408d11f1
Aarush Sharma <aarushsharma1016@gmail.com>
2026-01-02 17:05:13 +0530

    added a globe to the home page

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
+import { Globe } from "@/components/ui/globe"
 export default function Home() {
   return (
+    
     <main>
-      <h1>Welcome to my website</h1>
-      <p>This is built with Next.js</p>
+      <h1 className="text-4xl font-bold text-white">Welcome to my website</h1>
+      <p className="text-white">This is built with Next.js</p>
+      <Globe />
     </main>
   );
 }
